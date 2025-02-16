02e14944120495be2d0333bcc45a909e3c0d80a7
Amit Patil <amitsampat.patil@gmail.com>
2025-02-16 20:18:18 +0530

    Fixed issue

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
-
-const verifyToken = require('../middlewares/authMiddlewares');
 const obj = require('../controllors/activityControllor')
+const verifyToken = require('../middlewares/authMiddlewares');
+
 const router = express.Router();
-console.log(verifyToken)
+
 console.log(obj)
-console.log(router)
 
 router.post('/status/:id',verifyToken,obj.updateStatusOfActivity);
 
